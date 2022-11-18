00dd4301d2505cd5c1cdf96d80a8153f66ce338d
seijiiiiTakama <115197505+seijiiiiTakama@users.noreply.github.com>
2022-11-18 10:18:50 +0900

    リモートリポジトリでコンフリクト変更

--- PM1_2/PM1_2.cpp
+++ PM1_2/PM1_2.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello GitHub World\n";
 
-    std::cout << "Clone changed\n";
+    std::cout << "GitHub Clone changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
